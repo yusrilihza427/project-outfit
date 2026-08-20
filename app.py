c71c47fa1fdffa6f02db35154dd703958edc38b7
Yusril Ihza Alhafidz <yusrilihza427@gmail.com>
2026-08-20 17:53:11 +0700

    Prepare Flask app for Render

--- app.py
+++ app.py
@@ -49,4 +49,8 @@ def recommend():
 # RUN FLASK
 # ==========================
 if __name__ == "__main__":
-    app.run(debug=True)
+    import os
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
